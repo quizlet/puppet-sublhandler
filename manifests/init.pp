30cdcd75992a50d22c519408ff79cb55cb5cd7d5
Karoun Kasraie <karoun@quizlet.com>
2013-07-12 22:44:16 -0700

    Fixing tabs for linter

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # This is a placeholder class.
 class sublhandler {
-	package { 'SublHandler':
-		source   => 'SublHandler.app.zip',
-		provider => 'compressed_app'
-	}
+  package { 'SublHandler':
+    source   => 'SublHandler.app.zip',
+    provider => 'compressed_app'
+  }
 }
